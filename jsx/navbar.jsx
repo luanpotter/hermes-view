3729c96b684905c6f7fe03aa15c40dd861bbcdb3
Gabriel <Gabriel Soares>
2015-12-16 02:06:19 -0200

    fix navbar icon logo

--- jsx/navbar.jsx
+++ jsx/navbar.jsx
@@ -15,7 +15,6 @@ define(function (require) {
 
     				<div className='collapse navbar-collapse' id='bs-example-navbar-collapse-1'>
     					<ul className='nav navbar-nav'>
-    						<li><a href='#'>Link</a></li>
     					</ul>
     				</div>
 				</div>
