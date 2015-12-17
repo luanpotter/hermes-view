42ead9ff4ed7e942652d1a4fa8069d8ed2213fa3
Gabriel <Gabriel Soares>
2015-12-16 23:57:54 -0200

    server on mobile

--- jsx/server.jsx
+++ jsx/server.jsx
@@ -5,7 +5,7 @@ define(function (require) {
 	var Server = React.createClass({
 		render: function () {
 			var server = this.props.value;
-			return <div className='col-sm-3 list-servers'>
+			return <div className='col-sm-3 col-xs-6 list-servers'>
 				<div className='thumbnail list-group-item server'>
 					<Tooltip text={ server.ip }>{ server.label }</Tooltip>
 					<img className='server-status' alt={ server.status } src={'../images/server-' + server.status.toLowerCase() + '.png'}/>
